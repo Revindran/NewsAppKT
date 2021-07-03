78e7968a2cbdad2d3b69f6a6b2791ae8b84ce39d
Raveendran N <ravindran1307@gmail.com>
2021-07-04 05:25:45 +0530

    Base Functionalities done. next Pagination

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -1,13 +1,27 @@
 package com.androiddevs.mvvmnewsapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.repository.NewsRepository
+import com.androiddevs.mvvmnewsapp.utils.ArticleDatabase
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        val repository = NewsRepository(ArticleDatabase(this))
+        val newsViewModelProviderFactory = NewsViewModelProviderFactory(repository)
+        viewModel =
+            ViewModelProvider(this, newsViewModelProviderFactory).get(NewsViewModel::class.java)
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "93c9d4a433a84727916129e3fd0d6def"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+    }
+}
